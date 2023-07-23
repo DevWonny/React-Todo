e358485fea23590ba429eae2312107d1c4728246
DevWonny <cjfdnjs1994@naver.com>
2023-07-23 16:18:12 +0900

    Todo list input publishing

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,5 +1,18 @@
+import "./AddItem.scss";
 const AddItem = () => {
-  return <div>AddItem</div>;
+  return (
+    <>
+      <div className="add-item-wrap">
+        <input
+          type="text"
+          className="add-item"
+          id="add"
+          placeholder="할 일을 입력하세요."
+        />
+        <label htmlFor="add">+</label>
+      </div>
+    </>
+  );
 };
 
 export default AddItem;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import "./Header.scss";
 const Header = (props) => {
-  console.log("test");
   return (
     <>
       <div className="header-wrap">Todo List</div>
