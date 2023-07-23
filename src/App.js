5e6d7c6a80ceaf1e294f2119da7c6cd91965ac3e
Kevin <kevin@b2ggames.com>
2023-07-23 14:06:21 +0900

    feat : Start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "React";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name} Hello!</h1>
+      <h2>잘 보이니?</h2>
+      <button onClick={() => console.log("1111")}>Test</button>
+    </>
   );
 }
 
